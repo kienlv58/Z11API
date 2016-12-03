3afd5154e50c4ffa739d1c721fa529d33a73b51c
Kien <kienlv@newwayplus.com.vn>
2016-12-03 17:15:36 +0700

    asf

--- app/Http/Middleware/CheckAdmin.php
+++ app/Http/Middleware/CheckAdmin.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\User;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
@@ -16,9 +17,16 @@ class CheckAdmin
      */
     public function handle($request, Closure $next)
     {
-       if(Auth::check() or $request->user()->type != 'admin' or $request->user()->type != 'mod' ){
-           return response()->json(['code'=>400,'status'=>'you are not admin'],400);
-       }
-        return $next($request);
+        $uid = $request->input('uid');
+        $user = User::find($uid);
+        $a = $user->type;
+        if ($user == null){
+            return response()->json(['code'=>404,'status'=>'user null'],404);
+        }else if ($a === 'admin' || $a === 'mod'){
+            return $next($request);
+        }else{
+            return response()->json(['code'=>400,'status'=>'you are not admin or mod'],400);
+        }
+
     }
 }
